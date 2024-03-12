d7ec2517394dc22094b35694d9cbfe5c618d3385
XsixTonk <161140511+XsixTonk@users.noreply.github.com>
2024-03-13 00:33:01 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,17 +1,57 @@
 $(document).ready(function(){
+    // Function to create overlay
+    function createOverlay() {
+        // Create overlay div
+        var overlay = $('<div class="overlay"></div>');
+        
+        // Style overlay
+        overlay.css({
+            'position': 'fixed',
+            'top': '0',
+            'left': '0',
+            'width': '100%',
+            'height': '100%',
+            'background': 'rgba(0, 0, 0, 0.7)',
+            'z-index': '9999',
+            'display': 'none'
+        });
+        
+        // Create close button
+        var closeButton = $('<span class="close-button">&times;</span>');
+        
+        // Style close button
+        closeButton.css({
+            'position': 'absolute',
+            'top': '10px',
+            'right': '10px',
+            'color': '#fff',
+            'font-size': '20px',
+            'cursor': 'pointer',
+            'z-index': '99999'
+        });
+        
+        // Append close button to overlay
+        overlay.append(closeButton);
+        
+        // Append overlay to body
+        $('body').append(overlay);
+        
+        return overlay;
+    }
+    
     // When any iframe inside a video-card is clicked
     $('.video-card .video-iframe').on('click', function(){
         // Get the src of the clicked iframe
         var iframeSrc = $(this).attr('src');
         
-        // Create overlay div
-        var overlay = $('<div class="overlay" id="overlay"></div>');
+        // Create overlay
+        var overlay = createOverlay();
         
         // Create video container
         var videoContainer = $('<div class="video-container"></div>');
         
         // Create iframe with the same src
-        var iframe = $('<iframe width="560" height="315" src="' + iframeSrc + '" frameborder="0" allowfullscreen></iframe>');
+        var iframe = $('<iframe class="overlay-iframe" width="560" height="315" src="' + iframeSrc + '" frameborder="0" allowfullscreen></iframe>');
         
         // Append iframe to video container
         videoContainer.append(iframe);
@@ -19,43 +59,40 @@ $(document).ready(function(){
         // Append video container to overlay
         overlay.append(videoContainer);
         
-        // Append close button to overlay
-        overlay.append('<span class="close-button">&times;</span>');
-        
-        // Append overlay to body
-        $('body').append(overlay);
-        
         // Show overlay
         overlay.fadeIn();
         
         // Disable scrolling on body
         $('body').css('overflow', 'hidden');
         
-        // When overlay is clicked
-        overlay.on('click', function(){
+        // Prevent default action of iframe click
+        return false;
+    });
+    
+    // When close button is clicked
+    $('body').on('click', '.close-button', function(){
+        // Hide overlay
+        $('.overlay').fadeOut();
+        
+        // Remove src from overlay iframe
+        $('.overlay .overlay-iframe').attr('src', '');
+        
+        // Enable scrolling on body
+        $('body').css('overflow', 'auto');
+    });
+    
+    // When overlay is clicked
+    $('body').on('click', '.overlay', function(e){
+        // Check if the clicked element is the overlay itself
+        if ($(e.target).hasClass('overlay')) {
             // Hide overlay
             $(this).fadeOut();
             
-            // Remove overlay from DOM
-            $(this).remove();
+            // Remove src from overlay iframe
+            $(this).find('.overlay-iframe').attr('src', '');
             
             // Enable scrolling on body
             $('body').css('overflow', 'auto');
-        });
-        
-        // When close button is clicked
-        $('.close-button').on('click', function(){
-            // Hide overlay
-            overlay.fadeOut();
-            
-            // Remove overlay from DOM
-            overlay.remove();
-            
-            // Enable scrolling on body
-            $('body').css('overflow', 'auto');
-        });
-        
-        // Prevent default action of iframe click
-        return false;
+        }
     });
 });
